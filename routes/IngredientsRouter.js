e7737d507c93959db165f54975de14b612c2a2df
KarenEhab53 <karenehab53@gmail.com>
2026-05-13 09:29:41 +0300

    ingredients

--- routes/IngredientsRouter.js
+++ routes/IngredientsRouter.js
@@ -1,24 +1,19 @@
-const express = require ("express");
-
+const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../Middleware/authMiddleware");
+const isAdmin = require("../Middleware/adminMiddleware");
+const {
+  CreateIngredient,
+  EditIngredient,
+  FindIngredient,
+  FindAllIngredients,
+  DeleteIngredient,
+} = require("../Controllers/IngredientsController");
 
- const { authMiddleware , isAdmin } = require("../Middleware/authMiddleware");
-
-const {CreateIngredient,
-      EditIngredient,
-      FindIngredient,
-      FindAllIngredients,
-      DeleteIngredient} = require("../Controllers/IngredientsController");
-
-   
- 
-router.post("/create", authMiddleware ,CreateIngredient);
-router.get("/findAll",authMiddleware ,FindAllIngredients);
-router.patch("/edit/:id",authMiddleware ,EditIngredient);
-router.get("/find/:id", authMiddleware ,FindIngredient);
-router.delete("/delete/:id",authMiddleware ,isAdmin,DeleteIngredient);
-
-
-
+router.post("/create", authMiddleware, CreateIngredient);
+router.get("/findAll", authMiddleware, FindAllIngredients);
+router.patch("/edit/:id", authMiddleware, EditIngredient);
+router.get("/find/:id", authMiddleware, FindIngredient);
+router.delete("/delete/:id", authMiddleware, isAdmin, DeleteIngredient);
 
 module.exports = router;
